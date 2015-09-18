4544fd7624019b8eb5d78cd2694c52a150658454
Heather Ratcliffe <hratcliffe.glasgow@gmail.com>
2015-09-18 15:16:16 +0100

    Initial files, including XCode and pseudocode outline for project

---
+++ main.cpp
@@ -0,0 +1,32 @@
+//
+//  main.cpp
+//  
+//
+//  Created by Heather Ratcliffe on 18/09/2015.
+//
+//
+
+#include "main.h"
+//#include <math>
+#include <boost/math/special_functions.hpp>
+
+
+int main(int argc, char *argv[]){
+
+//Array class from std C++11 or boost
+//own multi dim version
+//version with axes
+
+
+//Test use and syntax of FFTW
+
+
+
+
+
+
+
+
+
+
+}

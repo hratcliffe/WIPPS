c1a14ba4e5202fe368922171f03da53065fe1a1d
Heather Ratcliffe <hratcliffe.glasgow@gmail.com>
2017-02-15 17:48:23 +0000

    Clean up, comments, name refactors

--- include/controller.h
+++ include/controller.h
@@ -18,7 +18,8 @@ class diffusion_coeff;
 
 /** \brief Controls plasma, spectrum and d_coeff objects and their connections
 *
-*This is the public facing class controlling plasma, spectrum and d_coeff objects. It makes sure there is a plasma to provide needed functions for the latters. Will also be responislbe for forming bounce averaged coefficients from a stack of ds?
+*This is the public facing class controlling plasma, spectrum and d_coeff objects. It makes sure there is a plasma to provide needed functions for the latters and keeps each D_coeff attached to the spectrum used to generate it. Should also be responsible for supplying D's in order to whatever does the bounce-averaging.
+*Because a spectrum is meaningless without a plasma, and  a diffusion coefficient meaningless without both a plasma and a spectrum, the controller class is the only thing allowed to create or destroy spectra and diffusion coefficients. Plasma's have other purposes so are not restricted in this way. When a new spectrum is created, the get_current_spectrum is set to refer to it. Any subsequent add_d operation will update the D linked to this spectrum.
 * @author Heather Ratcliffe @date 19/11/2015.
 */
 
@@ -26,30 +27,40 @@ typedef std::pair<spectrum*, diffusion_coeff*> spect_D_pair;
 
 class controller{
   plasma my_plas; /**< Plasma object*/
+  std::vector<spect_D_pair> spect_D_list;/**< pairs of spectrum and corresponding D*/
+  std::vector<diffusion_coeff *> d_specials_list;/**< Special D's for bounce averaging etc*/
+  size_t current_pair;/**<Index of current spectrum-D pair. Usually this is the last one added*/
+  size_t current_special_d;/**<Index of current special D object*/
 
-  std::vector<spect_D_pair> my_list;/**< pairs of spectrum and corresponding D*/
-  std::vector<diffusion_coeff *> d_specials;/**< Special D's for bounce averaging etc*/
-  
-  size_t current_spect;
-  size_t current_d;
-  void get_size(int dims[2]);
+/********Plasma, spectrum, D getters ****/
+  void get_D_size(int dims[2]);
+
 public:
 
+/********Basic setup functions ****/
   controller(std::string file_prefix);
   ~controller();
+  bool is_good(){return my_plas.is_good();}/**< Whether controller is fully setup*/
+
+/********Plasma, spectrum, D setup functions ****/
   void set_plasma_B0(my_type Bx_ref){my_plas.set_B0(Bx_ref);}
   bool add_spectrum(std::string file);
   bool add_spectrum(int nx, int n_ang,bool separable);
   bool add_d(int nx, int n_angs);
   void add_d_special(int nx, int n_angs);
+  
+/********Plasma, spectrum, D getters ****/
   spectrum * get_current_spectrum();
   diffusion_coeff * get_current_d();
   const plasma& get_plasma(){return my_plas;};
+
+/********Bounce averaging specials ****/
   void bounce_average();
   void handle_d_mpi();
+
+/********File IO functions ****/
   bool save_spectra(std::string pref);
   bool save_D(std::string pref);
-  bool is_good(){return my_plas.is_good();}/**< Whether controller is fully setup*/
 };
 
 
--- src/controller.cpp
+++ src/controller.cpp
@@ -20,15 +20,16 @@
 extern deck_constants my_const;
 extern const mpi_info_struc mpi_info;
 
+/********Basic setup functions ****/
 controller::controller(std::string file_prefix){
 /** \brief Setup
 *
-* Create plasma object and initialise
+* Create plasma object and initialise. A controller without a plasma is not meaningful. Plasma guarantees to be valid after construction, but may contain defaults if the specified file was not found. 
 */
 
   my_plas = plasma(file_prefix);
-  current_spect=0;
-  current_d=0;
+  current_pair=0;
+  current_special_d=0;
 }
 
 controller::~controller(){
@@ -36,25 +37,28 @@ controller::~controller(){
 *
 *Destroys all the D and spectrum objects
 */
-  for(size_t i=0; i<my_list.size(); i++){
-    if(my_list[i].first) delete my_list[i].first;
-    if(my_list[i].second) delete my_list[i].second;
-    my_list[i].first = nullptr;
-    my_list[i].second = nullptr;
+  for(size_t i=0; i<spect_D_list.size(); i++){
+    if(spect_D_list[i].first) delete spect_D_list[i].first;
+    if(spect_D_list[i].second) delete spect_D_list[i].second;
+    spect_D_list[i].first = nullptr;
+    spect_D_list[i].second = nullptr;
   }
 }
 
+/********Plasma, spectrum, D setup functions ****/
 bool controller::add_spectrum(std::string file){
-/** \brief Add spectrum from dump
+/** \brief Add spectrum from file
 *
-*Add spectrum read from file dump
+*Add spectrum read from a file, created using e.g. data_array::write_to_file or write_data in IDL
 */
+
+  //Read spectrum from file
   spectrum * tmp_spect = new spectrum(file);
   if(tmp_spect->is_good()){
+    //Assign controller and add to list
     tmp_spect->my_controller = this;
-    tmp_spect->init();
-    my_list.push_back(std::make_pair(tmp_spect, nullptr));
-    current_spect = my_list.size()-1;
+    spect_D_list.push_back(std::make_pair(tmp_spect, nullptr));
+    current_pair = spect_D_list.size()-1;
     return 0;
   }else{
     my_error_print("Spectrum construction failed", mpi_info.rank);
@@ -65,14 +69,16 @@ bool controller::add_spectrum(std::string file){
 bool controller::add_spectrum(int nx, int n_ang, bool separable){
 /** \brief Create and add spectrum
 *
-*If separable is true it is assumed the angle distrib does not depend on omega, else it does
+*Create a spectrum of the specified size and adds to list. See spectrum::spectrum(int n_om, int n_ang, bool separable) for details.
 */
+  //Create the spectrum
   spectrum * tmp_spect;
   tmp_spect = new spectrum(nx, n_ang, separable);
   if(tmp_spect->is_good()){
+    //Assign controller and add to list
     tmp_spect->my_controller = this;
-    my_list.push_back(std::make_pair(tmp_spect, nullptr));
-    current_spect = my_list.size()-1;
+    spect_D_list.push_back(std::make_pair(tmp_spect, nullptr));
+    current_pair = spect_D_list.size()-1;
     return 0;
 
   }else{
@@ -86,20 +92,25 @@ bool controller::add_d(int nx, int n_angs){
 *
 *Creates a diffusion coefficient of size nx x n_angs, paired with the last spectrum that was added.
 */
+  //Create empty diffusion coefficient
   diffusion_coeff * tmp_d;
   tmp_d = new diffusion_coeff(nx, n_angs);
 
   if(!tmp_d->is_good()) return 1;
 
+  //Create axes etc and add to list
   tmp_d->my_controller = this;
   tmp_d->make_velocity_axis();
   tmp_d->make_pitch_axis();
-  
-  my_list[current_spect].second = tmp_d;
+  spect_D_list[current_pair].second = tmp_d;
   return 0;
 }
 
 void controller::add_d_special(int nx, int n_angs){
+/** \brief Create and add special diffusion_coefficient
+*
+*Creates a diffusion coefficient of size nx x n_angs. Special coefficients do not have a matching spectrum. We use them mainly to hold results of bounce-averaging.
+*/
 
   diffusion_coeff * tmp_d;
   tmp_d = new diffusion_coeff(nx, n_angs);
@@ -107,16 +118,27 @@ void controller::add_d_special(int nx, int n_angs){
   tmp_d->make_velocity_axis();
   tmp_d->make_pitch_axis();
   
-  d_specials.push_back(tmp_d);
-  current_d = d_specials.size()-1;
+  d_specials_list.push_back(tmp_d);
+  current_special_d = d_specials_list.size()-1;
+}
+
+/********Plasma, spectrum, D getters ****/
+void controller::get_D_size(int dims[2]){
+/** \brief Get the dimensions of D
+*
+*Returns D dimensions in the dims array. Note that all D ought to be created the same size, but this is not guaranteed.
+*/
+  dims[0] = spect_D_list[current_pair].second->get_dims(0);
+  dims[1] = spect_D_list[current_pair].second->get_dims(1);
+
 }
 
 spectrum * controller::get_current_spectrum(){
 /** \brief Return current spectrum
 *
-*Returns pointer because spectra vector may be empty
+* Return a pointer to the current (i.e. the latest added) spectrum, or nullptr if list is empty.
 */
-  if(!my_list.empty()) return my_list[current_spect].first;
+  if(!spect_D_list.empty()) return spect_D_list[current_pair].first;
   else return nullptr;
 
 }
@@ -124,27 +146,28 @@ spectrum * controller::get_current_spectrum(){
 diffusion_coeff * controller::get_current_d(){
 /** \brief Return current D
 *
-*Returns pointer because D list may be empty
+* Return a pointer to the current (i.e. the latest added) d_coeff, or nullptr if list is empty.
 */
-  if(!my_list.empty()) return my_list[current_spect].second;
+  if(!spect_D_list.empty()) return spect_D_list[current_pair].second;
   else return nullptr;
 }
 
+/********Bounce averaging specials ****/
 void controller::bounce_average(){
 /** \brief Bounce average D
 *
-*Assumes the list contains D in order across space and performs bounce average to create special D. \todo Finish integrand \todo test case?
+*Assumes the list contains D in order across space and performs bounce average to create special D. \todo Finish integrand \todo test case? \todo Move these to dedicated code?
 */
-  if(my_list.size() ==0) return;
+  if(spect_D_list.size() ==0) return;
   //Empty list, nothing to do.
 
   int dims[2];
-  get_size(dims);
+  get_D_size(dims);
   
   add_d_special(dims[0], dims[1]);
   //Add new averaged D
-  d_specials[current_d]->tag = BOUNCE_AV;
-  strcpy(d_specials[current_d]->block_id, my_list[current_spect].second->block_id);
+  d_specials_list[current_special_d]->tag = BOUNCE_AV;
+  strcpy(d_specials_list[current_special_d]->block_id, spect_D_list[current_pair].second->block_id);
   //Tag as bounce av'd and copy block id from block used
 
   //Flatten down each d onto this one with all integrand included...
@@ -152,11 +175,11 @@ void controller::bounce_average(){
   for(int j=0; j< dims[0]; j++){
     for(int k=0; k<dims[1]; k++){
       val = 0;
-      for(size_t i=0; i<my_list.size() -1; i++){
-        val += my_list[i].second->get_element(j, k); //* integrand!
+      for(size_t i=0; i<spect_D_list.size() -1; i++){
+        val += spect_D_list[i].second->get_element(j, k); //* integrand!
         //Sum up the blocks done on this processor
       }
-      d_specials[current_d]->set_element(j, k, val);
+      d_specials_list[current_special_d]->set_element(j, k, val);
 
     }
   }
@@ -171,35 +194,29 @@ void controller::handle_d_mpi(){
 * Creates an MPI Summed d on the root node
 */
 
-//Make a reduced d on root, but leaving the current_d counter unchanged.
+//Make a reduced d on root, but leaving the current_special_d counter unchanged.
   int dims[2];
-  get_size(dims);
+  get_D_size(dims);
   int total = dims[0]*dims[1];
 
-  int prev_current_d = current_d;
+  int prev_current_d = current_special_d;
   if(mpi_info.rank ==0){
   //Add a new global value on root only
     add_d_special(dims[0], dims[1]);
     //Copy roots bounce average into this
-    *(d_specials[current_d]) = *(d_specials[prev_current_d]);
-    d_specials[current_d]->tag = GLOBAL;
+    *(d_specials_list[current_special_d]) = *(d_specials_list[prev_current_d]);
+    d_specials_list[current_special_d]->tag = GLOBAL;
   }
-  //reset current_d (affects root only)
-  int root_global_d = current_d;
-  current_d = prev_current_d;
+  //reset current_special_d (affects root only)
+  int root_global_d = current_special_d;
+  current_special_d = prev_current_d;
 
   //Reduce onto this new root d so we preserve all the local versions too
-  MPI_Reduce(get_current_d()->data, d_specials[root_global_d]->data , total, MPI_MYTYPE, MPI_SUM, 0, MPI_COMM_WORLD);
+  MPI_Reduce(get_current_d()->data, d_specials_list[root_global_d]->data , total, MPI_MYTYPE, MPI_SUM, 0, MPI_COMM_WORLD);
   
 }
 
-void controller::get_size(int dims[2]){
-
-  dims[0] = my_list[current_spect].second->get_dims(0);
-  dims[1] = my_list[current_spect].second->get_dims(1);
-
-}
-
+/********File IO functions ****/
 bool controller::save_spectra(std::string pref){
 /** \brief Save spectra to files (one per chunk)
 *
@@ -208,11 +225,11 @@ bool controller::save_spectra(std::string pref){
 
   std::fstream file;
   std::string filename, tmp;
-  for(size_t i=0; i<my_list.size(); ++i){
-    tmp = my_list[i].first->block_id;
-    filename = pref+"spec_"+tmp +"_"+mk_str(my_list[i].first->time[0]) + "_"+mk_str(my_list[i].first->time[1])+"_"+mk_str(my_list[i].first->space[0])+"_"+mk_str(my_list[i].first->space[1])+".dat";
+  for(size_t i=0; i<spect_D_list.size(); ++i){
+    tmp = spect_D_list[i].first->block_id;
+    filename = pref+"spec_"+tmp +"_"+mk_str(spect_D_list[i].first->time[0]) + "_"+mk_str(spect_D_list[i].first->time[1])+"_"+mk_str(spect_D_list[i].first->space[0])+"_"+mk_str(spect_D_list[i].first->space[1])+".dat";
     file.open(filename.c_str(),std::ios::out|std::ios::binary);
-    if(file.is_open()) my_list[i].first->write_to_file(file);
+    if(file.is_open()) spect_D_list[i].first->write_to_file(file);
     else return 1;
     file.close();
   }
@@ -229,25 +246,25 @@ bool controller::save_D(std::string pref){
   std::string filename, tmp;
 
   //Do local list
-  for(size_t i=0; i<my_list.size(); ++i){
-    tmp = my_list[i].second->block_id;
+  for(size_t i=0; i<spect_D_list.size(); ++i){
+    tmp = spect_D_list[i].second->block_id;
     //They might have different blocks
-    filename = pref+"D_"+tmp +"_"+mk_str(my_list[i].second->time[0]) + "_"+mk_str(my_list[i].second->time[1])+"_"+mk_str(my_list[i].second->space[0])+"_"+mk_str(my_list[i].second->space[1])+".dat";
+    filename = pref+"D_"+tmp +"_"+mk_str(spect_D_list[i].second->time[0]) + "_"+mk_str(spect_D_list[i].second->time[1])+"_"+mk_str(spect_D_list[i].second->space[0])+"_"+mk_str(spect_D_list[i].second->space[1])+".dat";
     //Don't bother saving these...
     
     file.open(filename.c_str(),std::ios::out|std::ios::binary|std::ios::in|std::ios::trunc);
-    if(file.is_open()) my_list[i].second->write_to_file(file);
+    if(file.is_open()) spect_D_list[i].second->write_to_file(file);
     else return 1;
     file.close();
   
   }
 
   //Save global D from root only
-  int d_global = d_specials.size()-1;
+  int d_global = d_specials_list.size()-1;
   if(mpi_info.rank == 0 ){
-    filename = pref+"D_"+tmp +"_"+mk_str(d_specials[d_global]->time[0]) + "_"+mk_str(d_specials[d_global]->time[1])+"_global.dat";
+    filename = pref+"D_"+tmp +"_"+mk_str(d_specials_list[d_global]->time[0]) + "_"+mk_str(d_specials_list[d_global]->time[1])+"_global.dat";
     file.open(filename.c_str(),std::ios::out|std::ios::binary|std::ios::in|std::ios::trunc);
-    if(file.is_open()) d_specials[d_global]->write_to_file(file);
+    if(file.is_open()) d_specials_list[d_global]->write_to_file(file);
     else return 1;
     file.close();
   }
